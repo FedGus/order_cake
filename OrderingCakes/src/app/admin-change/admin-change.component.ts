b03ad9af0dc81e69cd617aead615a065231a0873
FedGus <gusevfedya@mail.ru>
2020-06-17 13:51:38 +0300

    CRUD в админке

--- OrderingCakes/src/app/admin-change/admin-change.component.ts
+++ OrderingCakes/src/app/admin-change/admin-change.component.ts
@@ -11,6 +11,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class AdminChangeComponent implements OnInit {
   formAdd: FormGroup;
   cakes;
+  id:number;
   disabled = false;
   constructor(private activeRoute: ActivatedRoute,
     private router: Router,
@@ -21,7 +22,7 @@ export class AdminChangeComponent implements OnInit {
     this.updateData();
     this.formAdd = new FormGroup({                              /**создание новой формы для заказа тортов из списка готовых */ 
       name: new FormControl( { value: '', disabled: this.disabled }, [Validators.required]),
-      picture: new FormControl( { value: '', disabled: this.disabled }, [Validators.required]),
+      picture: new FormControl( { value: '', disabled: this.disabled }),
       info: new FormControl( { value: '', disabled: this.disabled } , [Validators.required]),
       price: new FormControl({ value: '', disabled: this.disabled }, [Validators.required]),
       });
@@ -40,9 +41,36 @@ export class AdminChangeComponent implements OnInit {
     }
   }
   onSubmit(){    
-console.log(this.formAdd.value);
+    if (this.id== null){
          this.CakesService.postNotes(this.formAdd.value);
-         this.router.navigate(['/admin-change']); 
-      
+    }
+    else {
+      this.CakesService.putById(this.id, this.formAdd.value);
+    }
+         this.updateData(); 
+         this.formAdd = new FormGroup({                              /**создание новой формы для заказа тортов из списка готовых */ 
+          name: new FormControl( { value: '', disabled: this.disabled }, [Validators.required]),
+          picture: new FormControl( { value: '', disabled: this.disabled }),
+          info: new FormControl( { value: '', disabled: this.disabled } , [Validators.required]),
+          price: new FormControl({ value: '', disabled: this.disabled }, [Validators.required]),
+          });
+    
+    }
+
+    delete(id:number) {
+      console.log(id);
+      this.CakesService.deleteById(id);
+      this.updateData(); 
+    }
+
+    edit(cake){
+      console.log(cake);
+      this.formAdd = new FormGroup({                              /**создание новой формы для заказа тортов из списка готовых */ 
+        name: new FormControl( { value: cake.name, disabled: this.disabled }, [Validators.required]),
+        picture: new FormControl( { value: cake.picture, disabled: this.disabled }),
+        info: new FormControl( { value: cake.info, disabled: this.disabled } , [Validators.required]),
+        price: new FormControl({ value: cake.price, disabled: this.disabled }, [Validators.required]),
+        });
+        this.id = cake.id;
     }
 }
